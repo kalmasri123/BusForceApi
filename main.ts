8e393ae6881241355996fb1a5fb69b27f4d1bfe2
kareem <kalmasri@me.com>
2022-11-17 17:38:52 -0500

    cors

--- main.ts
+++ main.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import { GetTimeToStop } from './controllers/StopController';
 import mongoose from 'mongoose';
+import cors from 'cors'
 console.log(process.env.MONGOURI)
 mongoose.connect(process.env.MONGOURI as string).then(res=>{
     console.log("Connected")
 })
 const app = express()
-
+app.use(cors())
 app.get("/stops/timetostop/:startStop/:endStop",GetTimeToStop)
 app.listen(2100)
